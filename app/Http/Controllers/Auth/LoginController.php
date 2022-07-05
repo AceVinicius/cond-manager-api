1a350d29160da96d6055f648ff346d95df8f264f
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2022-07-05 19:07:03 -0300

    Implemented Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -25,6 +27,37 @@ class LoginController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $credentials = [
+            'cpf' => $request['cpf'],
+            'password' => $request['password'],
+        ];
+
+        if (! Auth::attempt($credentials)) {
+            $response = ['message' => 'Invalid credentials.'];
+            return response()->json($response, 401);
+        }
+
+        $user = User::where('cpf', $request['cpf'])->first();
+
+        if (! $user) {
+            $response = ['message' => 'Error finding user.'];
+            return response()->json($response, 500);
+        }
+
+        $response = [
+            'message' => 'User logged in successfully.',
+            'token' => $user->createToken(time().rand(0,9999))->plainTextToken,
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'cpf' => $user->cpf,
+                'created_at' => $user->created_at,
+                'updated_at' => $user->updated_at,
+                'properties' => $user->units(),
+            ],
+        ];
+
+        return response()->json($response, 200);
     }
 }
